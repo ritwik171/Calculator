8471374e57128d2fbdda432d56a1851e19c149e5
RITWIK DESHPANDE <RITWIK DESHPANDE@DESKTOP-A96MTDF>
2018-07-02 15:49:28 +0530

    Dev-101 By Ritwik

---
+++ src/calculator/UseMe.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculator;
+
+/**
+ *
+ * @author RITWIK DESHPANDE
+ */
+public class UseMe {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int result = Demo.add(10, 20);
+        System.out.println(result);
+    }
+    
+}
